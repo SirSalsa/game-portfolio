fde74867d774e8c607ac5fb5e29f0f1492b1a8e0
SirSalsa <46669698+SirSalsa@users.noreply.github.com>
2023-04-01 22:48:12 +0200

    Jobs WIP

--- src/modules/jobs/jobs.tsx
+++ src/modules/jobs/jobs.tsx
@@ -1,7 +1,28 @@
+import "./jobs.scss"
+import candycrushlogo from "../../media/candy_crush_logo.jpg"
+import petrescuelogo from "../../media/petrescuelogo.png"
+
 function Jobs() {
     return(
         <div className="Jobs_wrapper">
-            <p>swag</p>
+            <div id="header_container">
+                <h1>Professional Projects</h1>
+                <p>Click on a tile to learn more about a project.</p>
+            </div>
+            <div id="projects_container">
+                <article>
+                    <img src={candycrushlogo} alt="Candy Crush Saga Logo" />
+                    <h3>Candy Crush Saga</h3>
+                    <h4>King</h4>
+                    <p></p>
+                </article>
+                <article>
+                    <img src={petrescuelogo} alt="Pet Rescue Saga Logo" />
+                    <h3>Pet Rescue Saga</h3>
+                    <h4>King</h4>
+                    <p></p>
+                </article>
+            </div>
         </div>
     );
 }
