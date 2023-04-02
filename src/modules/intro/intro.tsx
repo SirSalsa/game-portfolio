6cbbc54ec5807d09825c172d86054f6e1273400b
SirSalsa <46669698+SirSalsa@users.noreply.github.com>
2023-04-02 16:46:59 +0200

    Finished About page

--- src/modules/intro/intro.tsx
+++ src/modules/intro/intro.tsx
@@ -1,8 +1,6 @@
 import "./intro.scss"
 import portrait from "../../media/portrait.png"
-import susimg from "../../media/sus.png"
-import controller from "../../media/controller.png"
-import code from "../../media/code.png"
+import cv_url from "../../media/cv.png"
 
 function Intro() {
     return(
@@ -14,27 +12,15 @@ function Intro() {
             <div id="portrait_container">
                 <img src={portrait} alt="Portrait" />
             </div>
-            <div className="About_wrapper">
-                <div id="desc_container">
-                    <h2>About Me</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    </div>
-                <div id="background_container">
-                    <article>
-                        <img src={controller} id="background_img"/>
-                        <div>
-                            <h3>Part Designer</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                        </div>
-                    </article>
-                    <article>
-                        <img src={code} id="background_img"/>
-                        <div>
-                            <h3>Part Programmer</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                        </div>
-                    </article>
-                </div>
+            <div id="desc_container">
+                <h2>About Me</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <form id="cv_button_wrapper" target="_blank">
+                    <button id="about_button" formAction="https://drive.google.com/file/d/18Mti1pgJeeQZ0ci-s3oold7Ah3ZnK3iV/view?usp=share_link">
+                        <img src={cv_url} alt="CV icon" />
+                        <p>Download CV</p>
+                    </button>
+                </form>
             </div>
         </div>
     );
