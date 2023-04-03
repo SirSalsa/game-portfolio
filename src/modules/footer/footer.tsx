553e3fcefa636b792fa10f1b0d46629e9ab72520
SirSalsa <46669698+SirSalsa@users.noreply.github.com>
2023-04-03 10:09:21 +0200

    Added legal

--- src/modules/footer/footer.tsx
+++ src/modules/footer/footer.tsx
@@ -1,21 +1,28 @@
 import "./footer.scss"
 import linkedin from "./media/linkedin.png"
+import email from "./media/email.png"
+import phone from "./media/phone.png"
 
 function Footer() {
     return(
         <div className="Footer_wrapper">
             <h1>Contact</h1>
-            <p>Want to get in touch? Here are a few ways you can connect with me.</p>
+            <p id="contact_desc">Want to get in touch? Here are a few ways you can connect with me.</p>
             <div id="contactinfo_container">
                 <a href="https://www.linkedin.com/in/torsten-lareke/">
-                    <img width="35px" src={linkedin} alt="Linkedin img" />
+                    <img src={linkedin} alt="Linkedin img" />
                 </a>
-                <a href="https://www.linkedin.com/in/torsten-lareke/">
-                    <img width="35px" src={linkedin} alt="Linkedin img" />
+                <a href="mailto:torsten.lareke@hotmail.se">
+                    <img src={email} alt="Email img" />
                 </a>
-                <a href="https://www.linkedin.com/in/torsten-lareke/">
-                    <img width="35px" src={linkedin} alt="Linkedin img" />
+                <a href="tel:+46-73268-2109">
+                    <img src={phone} alt="Phone img" />
                 </a>
+            </div>
+            
+            <div id="legal_text">
+                <p>Copyright © 2023 by Torsten Lareke. All rights reserved.</p>
+                <p>Any games or trademarks shown on this website are the property of their respective owners.</p>
             </div>
         </div>
     );
