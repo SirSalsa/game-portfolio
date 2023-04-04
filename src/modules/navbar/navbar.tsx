d4d5496e0fe2b69f1a8a86244b56a85efd3be7d6
SirSalsa <46669698+SirSalsa@users.noreply.github.com>
2023-04-04 21:03:04 +0200

    Added click to scroll function on navbar

--- src/modules/navbar/navbar.tsx
+++ src/modules/navbar/navbar.tsx
@@ -1,24 +1,23 @@
 import "./navbar.scss"
 
 function Navbar() {
+    const scrollToClass = (className: string) => {
+        const target = document.querySelector(`.${className}`) as HTMLElement;
+        if (target) {
+            target.scrollIntoView({ behavior: 'smooth' });
+        }
+    };
+
     return(
         <div className="Navbar_wrapper">
             <div id="left_container">
                 <h1>Torsten Lareke</h1>
             </div>
             <div id="right_container">
-                <article>
-                    <h2>About</h2>
-                </article>
-                <article>
-                    <h2>Professional Projects</h2>
-                </article>
-                <article>
-                    <h2>Other Projects</h2>
-                </article>
-                <article>
-                    <h2>Contact</h2>
-                </article>
+                <h2 onClick={() => scrollToClass('Intro_wrapper')}>About</h2>
+                <h2 onClick={() => scrollToClass('PP_wrapper')}>Professional Projects</h2>
+                <h2 onClick={() => scrollToClass('OP_wrapper')}>Other Projects</h2>
+                <h2 onClick={() => scrollToClass('Footer_wrapper')}>Contact</h2>
             </div>
         </div>
     );
