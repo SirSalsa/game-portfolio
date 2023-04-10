9f5e06bbc4770499633243b3b13eb7ef4cfe7081
SirSalsa <46669698+SirSalsa@users.noreply.github.com>
2023-04-11 00:05:59 +0200

    Added tags, navbar border

--- src/modules/jobs/jobs.tsx
+++ src/modules/jobs/jobs.tsx
@@ -4,6 +4,7 @@ import petrescuelogo from "./media/professional/petrescuelogo.png"
 import roofRunners from "./media/other/roofrunners.jpg"
 import laborated from "./media/other/laborated.png"
 
+
 function Jobs() {
     return(
         <main>
@@ -15,11 +16,18 @@ function Jobs() {
                 <div id="projects_container">
                     <article>
                         <img src={candycrushlogo} alt="Candy Crush Saga Logo" />
+                        <div className="tag_container">
+                            <p id="tag_1">Level Design</p>
+                            <p id="tag_2">Game Design</p>
+                        </div>
                         <h3>Candy Crush Saga</h3>
                         <h4>King</h4>
                     </article>
                     <article>
                         <img src={petrescuelogo} alt="Pet Rescue Saga Logo" />
+                        <div className="tag_container">
+                            <p id="tag_1">Level Design</p>
+                        </div>
                         <h3>Pet Rescue Saga</h3>
                         <h4>King</h4>
                     </article>
@@ -33,11 +41,20 @@ function Jobs() {
                 <div id="projects_container">
                     <article>
                         <img src={roofRunners} alt="Roof Runners Logo" />
+                        <div className="tag_container">
+                            <p id="tag_1">Level Design</p>
+                            <p id="tag_2">Game Design</p>
+                            <p id="tag_3">Unity</p>
+                        </div>
                         <h3>Roof Runners</h3>
                         <h4>School Project</h4>
                     </article>
                     <article>
                         <img src={laborated} alt="Laborated Logo" />
+                        <div className="tag_container">
+                            <p id="tag_1">Level Design</p>
+                            <p id="tag_2">Game Design</p>
+                        </div>
                         <h3>Laborated</h3>
                         <h4>School Project</h4>
                     </article>
