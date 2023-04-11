fdbf17b11d0532c967206348f158d99336401fa4
SirSalsa <46669698+SirSalsa@users.noreply.github.com>
2023-04-11 17:22:03 +0200

    Mapping to projectWindow now works

--- src/modules/jobs/jobs.tsx
+++ src/modules/jobs/jobs.tsx
@@ -106,75 +106,83 @@ function Jobs() {
         setSelectedProject(null);
     };
 
-    return(
+    return (
         <main>
-            <div className="PP_wrapper">
-                <div id="header_container">
-                    <h1>Professional Projects</h1>
-                    <p>Click on a tile to learn more about a project.</p>
-                </div>
-                <div id="projects_container">
-                    {PP_projects.map((project, index) => (
-                        <article key={index} onClick={() => handleProjectClick(index)}>
-                            <img src={project.imageSrc} alt="Candy Crush Saga Logo" />
-                            <div className="tag_container">
-                                {project.tags.map((tag) => (
-                                    <p key={tag.id} id={tag.id}>
-                                    {tag.text}
-                                    </p>
-                                ))}
-                            </div>
-                            <h3>{project.title}</h3>
-                            <h4>{project.subtitle}</h4>
-                        </article>
-                     ))}
-                </div>
+          <div className="PP_wrapper">
+            <div id="header_container">
+              <h1>Professional Projects</h1>
+              <p>Click on a tile to learn more about a project.</p>
             </div>
-            <div className="OP_wrapper">
-                <div id="header_container">
-                    <h1>Other Projects</h1>
-                    <p>Click on a tile to learn more about a project.</p>
-                </div>
-                <div id="projects_container">
-                {OP_projects.map((project, index) => (
-                        <article key={index} onClick={() => handleProjectClick(index)}>
-                            <img src={project.imageSrc} alt="Candy Crush Saga Logo" />
-                            <div className="tag_container">
-                                {project.tags.map((tag) => (
-                                    <p key={tag.id} id={tag.id}>
-                                    {tag.text}
-                                    </p>
-                                ))}
-                            </div>
-                            <h3>{project.title}</h3>
-                            <h4>{project.subtitle}</h4>
-                        </article>
-                     ))}
-                </div>
-            </div>
-            {selectedProject !== null && (
-                <div className="ProjectWindow_wrapper">
-                    <button onClick={handleCloseClick}>Close</button>
-                    <h1>{PP_projects[selectedProject].title}</h1>
-                    <img src={PP_projects[selectedProject].imageSrc} alt={PP_projects[selectedProject].title} />
-                <div className="tag_container">
-                     {PP_projects[selectedProject].tags.map((tag) => (
-                        <p id={tag.id} key={tag.id}>{tag.text}</p>
+            <div id="projects_container">
+              {PP_projects.map((project, index) => (
+                <article key={index} onClick={() => handleProjectClick(index)}>
+                  <img src={project.imageSrc} alt="Project Thumbnail" />
+                  <div className="tag_container">
+                    {project.tags.map((tag) => (
+                      <p key={tag.id} id={tag.id}>
+                        {tag.text}
+                      </p>
                     ))}
-                </div>
-                <p id="PW_desc">{PP_projects[selectedProject].description}</p>
-                <div id="PW_overview">
-                    <h4>What I did:</h4>
-                    <ul>
-                        {PP_projects[selectedProject].overview.map((task, index) => (
-                            <li key={index}>{task}</li>
-                        ))}
-                    </ul>
-                </div>
+                  </div>
+                  <h3>{project.title}</h3>
+                  <h4>{project.subtitle}</h4>
+                </article>
+              ))}
             </div>
-            )}
+          </div>
+          <div className="OP_wrapper">
+            <div id="header_container">
+              <h1>Other Projects</h1>
+              <p>Click on a tile to learn more about a project.</p>
+            </div>
+            <div id="projects_container">
+              {OP_projects.map((project, index) => (
+                <article key={index} onClick={() => handleProjectClick(index + PP_projects.length)}>
+                  <img src={project.imageSrc} alt="Project Thumbnail" />
+                  <div className="tag_container">
+                    {project.tags.map((tag) => (
+                      <p key={tag.id} id={tag.id}>
+                        {tag.text}
+                      </p>
+                    ))}
+                  </div>
+                  <h3>{project.title}</h3>
+                  <h4>{project.subtitle}</h4>
+                </article>
+              ))}
+            </div>
+          </div>
+          {selectedProject !== null && (
+            <div className="ProjectWindow_wrapper">
+              <button onClick={handleCloseClick}>Close</button>
+              <h1>{selectedProject < PP_projects.length ? PP_projects[selectedProject].title : OP_projects[selectedProject - PP_projects.length].title}</h1>
+              <img src={selectedProject < PP_projects.length ? PP_projects[selectedProject].imageSrc : OP_projects[selectedProject - PP_projects.length].imageSrc} alt="Project Thumbnail" />
+              <div className="tag_container">
+                {selectedProject < PP_projects.length ? PP_projects[selectedProject].tags.map((tag) => (
+                  <p id={tag.id} key={tag.id}>
+                    {tag.text}
+                  </p>
+                )) : OP_projects[selectedProject - PP_projects.length].tags.map((tag) => (
+                  <p id={tag.id} key={tag.id}>
+                    {tag.text}
+                  </p>
+                ))}
+              </div>
+              <p id="PW_desc">{selectedProject < PP_projects.length ? PP_projects[selectedProject].description : OP_projects[selectedProject - PP_projects.length].description}</p>
+              <div id="PW_overview">
+                <h4>What I did:</h4>
+                <ul>
+                  {selectedProject < PP_projects.length ? PP_projects[selectedProject].overview.map((task, index) => (
+                    <li key={index}>{task}</li>
+                  )) : OP_projects[selectedProject - PP_projects.length].overview.map((task, index) => (
+                    <li key={index}>{task}</li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          )}
         </main>
-    );
+      );
 }
 
 export default Jobs
