28d1928e087a4531b7580f907d3f24ec2be64b11
SirSalsa <46669698+SirSalsa@users.noreply.github.com>
2023-04-11 15:47:31 +0200

    Added temporary projectwindows

--- src/modules/jobs/jobs.tsx
+++ src/modules/jobs/jobs.tsx
@@ -3,9 +3,20 @@ import candycrushlogo from "./media/professional/candy_crush_logo.jpg"
 import petrescuelogo from "./media/professional/petrescuelogo.png"
 import roofRunners from "./media/other/roofrunners.jpg"
 import laborated from "./media/other/laborated.png"
-
+import { useState } from "react"
 
 function Jobs() {
+
+    const [showWindow, setShowWindow] = useState(false);
+
+    const handleProjectClick = () => {
+        setShowWindow(true);
+    };
+
+    const handleCloseClick = () => {
+        setShowWindow(false);
+    };
+
     return(
         <main>
             <div className="PP_wrapper">
@@ -14,7 +25,7 @@ function Jobs() {
                     <p>Click on a tile to learn more about a project.</p>
                 </div>
                 <div id="projects_container">
-                    <article>
+                    <article onClick={handleProjectClick}>
                         <img src={candycrushlogo} alt="Candy Crush Saga Logo" />
                         <div className="tag_container">
                             <p id="tag_1">Level Design</p>
@@ -60,6 +71,32 @@ function Jobs() {
                     </article>
                 </div>
             </div>
+            {showWindow && (
+                <div className="ProjectWindow_wrapper">
+                    <h1>Project Title</h1>
+                    <img src={candycrushlogo} alt="Candy Crush Saga Logo"/>
+                    <div className="tag_container">
+                        <p id="tag_1">Level Design</p>
+                        <p id="tag_2">Game Design</p>
+                    </div>
+                    <p id="PW_desc">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    </p>
+                    <div id="PW_overview">
+                        <h4>What I did:</h4>
+                        <ul>
+                            <li>Feeding the dog.</li>
+                            <li>Getting coffee to my seniors.</li>
+                            <li>Pushing buttons everyday.</li>
+                            <li>Stealing milk from the cafeteria.</li>
+                            <li>Feeding the dog.</li>
+                            <li>Getting coffee to my seniors.</li>
+                            <li>Pushing buttons everyday.</li>
+                            <li>Stealing milk from the cafeteria.</li>
+                        </ul>
+                    </div>
+                </div>
+            )}
         </main>
     );
 }
