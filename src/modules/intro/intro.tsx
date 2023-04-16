94f94c28fd531e3789a03b971af233f18683a798
SirSalsa <46669698+SirSalsa@users.noreply.github.com>
2023-04-16 18:53:50 +0200

    Added about me text

--- src/modules/intro/intro.tsx
+++ src/modules/intro/intro.tsx
@@ -3,7 +3,7 @@ import portrait from "./media/portrait.png"
 import cv_url from "./media/cv.png"
 
 function Intro() {
-    return(
+    return (
         <div className="Intro_wrapper">
             <div id="title_container">
                 <h1>Torsten Lareke</h1>
@@ -14,7 +14,13 @@ function Intro() {
             </div>
             <div id="desc_container">
                 <h2>About Me</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p>
+                    Hello there! I'm Torsten, a level designer and backend developer with a strong passion for creating unforgettable game experiences. With almost two years of experience in the games industry, I honed my design skills working on casual mobile games.
+
+                    In addition to my design work, I gained experience in backend C# development, where I learned skills in database management, API integration, and more. Combining my design skills with my programming capabilities, I'm able to create better and more engaging experiences in games.
+
+                    I'm always up for a challenge and eager to try out new things, so feel free to reach out if you're interested in collaborating on a project. Let's create something amazing together!
+                </p>
                 <form id="cv_button_wrapper" target="_blank">
                     <button id="about_button" formAction="https://drive.google.com/file/d/18Mti1pgJeeQZ0ci-s3oold7Ah3ZnK3iV/view?usp=share_link">
                         <img src={cv_url} alt="CV icon" />
